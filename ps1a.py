1524a46c1360096add8d47a9c57d5b7ae66b0dcb
RishithasriA <rishithasriamari1310@gmail.com>
2026-02-22 11:45:59 +0530

    Fix input prompts and output formatting in ps1a.py

--- ps1a.py
+++ ps1a.py
@@ -1,6 +1,6 @@
-annual_salary=float(input("enter yourannual salary:"))
-portion_saved=float(input("enter the percent of your salary to save , as a decimal :"))
-cost_of_dreamhouse=float(input("enter thecost of your dream house :"))
+annual_salary=float(input("Enter your annual salary:"))
+portion_saved=float(input("Enter the percent of your salary to save , as a decimal :"))
+cost_of_dreamhouse=float(input("Enter the cost of your dream house :"))
 #down_payment(25% of total_cost)
 down_payment=0.25*cost_of_dreamhouse
 current_savings=0.0
@@ -14,4 +14,5 @@ months=0
 while current_savings<down_payment:
     current_savings+=monthly_saving+(current_savings*r)
     months+=1
-print(f'number of montha:{months}')    
+
+print(f'Number of months:{months}')    
