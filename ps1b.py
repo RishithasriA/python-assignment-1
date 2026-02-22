fd629c07638d190cd7994387c69ff85fc81de8b7
RishithasriA <rishithasriamari1310@gmail.com>
2026-02-22 11:59:19 +0530

    Fix input prompts and calculation logic in ps1b.py

--- ps1b.py
+++ ps1b.py
@@ -1,7 +1,7 @@
 annual_salary=float(input("Enter your starting annual salary:"))
-portion_saved=float(input("Enter the pecent of your salary to save,as a decimal:"))
+portion_saved=float(input("Enter the percent of your salary to save,as a decimal:"))
 total_cost=float(input('Enter the cost of your dream house:'))
-semi_annual_raise=float(input("Enter the semi annual raise,as adecimal:"))
+semi_annual_raise=float(input("Enter the semi annual raise,as a decimal:"))
 #down payment
 down_payment=0.25*total_cost 
 current_savings=0.0
@@ -16,7 +16,8 @@ while current_savings<down_payment:
     current_savings+=monthly_saving+(current_savings*r)
     months+=1
     if months%6==0:
-        annual_salary+=annual_salary*semi_raise
+        annual_salary+=annual_salary*semi_annual_raise
         monthly_salary=annual_salary/12
 print(f"Number of months:{months}")
+
 
