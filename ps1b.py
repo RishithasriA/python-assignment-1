ff5f5228b1df873cbd393b7685bdae46aaf31d66
RishithasriA <rishithasriamari1310@gmail.com>
2026-02-22 11:56:11 +0530

    Implement savings calculation with raises
    Added annual return and semi-annual raise logic.

--- ps1b.py
+++ ps1b.py
@@ -5,6 +5,7 @@ semi_annual_raise=float(input("Enter the semi annual raise,as adecimal:"))
 #down payment
 down_payment=0.25*total_cost 
 current_savings=0.0
+annual_return=0.04
 #current monthly salary
 monthly_salary=annual_salary/12
 monthly_saving=portion_saved*monthly_salary
@@ -14,5 +15,8 @@ months=0
 while current_savings<down_payment:
     current_savings+=monthly_saving+(current_savings*r)
     months+=1
-
+    if months%6==0:
+        annual_salary+=annual_salary*semi_raise
+        monthly_salary=annual_salary/12
 print(f"Number of months:{months}")
+
